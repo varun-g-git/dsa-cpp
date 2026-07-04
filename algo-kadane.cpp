7e57dfa3dc8345619c7fbc4fa323ce6671dbc483
varun <varungoutham10@gmail.com>
2026-07-04 17:06:52 +0530

    Create algo-kadane.cpp

---
+++ algo-kadane.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& arr) {
+        int n=arr.size();
+        int sum=0;
+        int maxi=INT_MIN;
+        for(int i=0;i<n;i++){
+        
+                sum+=arr[i];
+    
+            if(sum>maxi){
+                maxi=sum;
+            }
+            if(sum<0){
+                sum=0;
+            }
+
+        }
+       
+        return maxi;
+        
+    }
+};
